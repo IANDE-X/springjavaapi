c4720ae744baf1bedc4fd5f307f2309aa5de53e7
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-06-02 13:38:30 +0200

    Update UserController.java

--- src/main/java/dev/ian/controller/UserController.java
+++ src/main/java/dev/ian/controller/UserController.java
@@ -24,38 +24,7 @@ public class UserController {
         return userService.list();
     }
 
-//
-//    //gettt
-//    @GetMapping("/list/{id}")
-//    public ResponseEntity<User> get(@PathVariable Long id) {
-//        try {
-//            User user = userService.get(id);
-//            return new ResponseEntity<User>(user, HttpStatus.OK);
-//        } catch (ResourceNotFoundException e) {
-//            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
-//        }
-//    }
-//    //createeeeeee
-//    @PostMapping("/list")
-//    public void add(@RequestBody User user) {
-//        userService.save(user);
-//    }
-//    //updateeeeeeeeeee
-//    @PutMapping("/list/{id}")
-//    public ResponseEntity<?> update(@RequestBody User user, @PathVariable Long id) {
-//        try {
-//            User existUser = userService.get(id);
-//            userService.save(user);
-//            return new ResponseEntity<>(HttpStatus.OK);
-//        } catch (ResourceNotFoundException e) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//    }
-//    //deleteeeeeeeeeeee
-//    @DeleteMapping("/list/{id}")
-//    public void delete(@PathVariable Long id) {
-//        userService.delete(id);
-//    }
+
 ////
     //creating a get mapping that retrieves the detail of a specific user----->working
 @GetMapping("/user/{id}")
@@ -84,65 +53,4 @@ private User getUser(@PathVariable("id") Long id)
         return user;
     }
 }
-//    @DeleteMapping("{/users/id}")
-//    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id) {
-//
-//        User user = userRepository.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException(" the Employe being serached does not exist with id: " + id));
-//
-//        userRepository.delete(user);
-//
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//
-//    }
-
-
-//
-//    //the create user REST API
-//    @PostMapping
-//    public User createUser@RequestBody User user) {
-//        return userRepository.save(user);
-//    }
-//
-//    //  the get employee by id REST API
-//    @GetMapping("{id}")
-//    public ResponseEntity<User> getUserById(@PathVariable long id) {
-//        User user= user.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException("user  not exist with id:" + id));
-//        return ResponseEntity.ok(user);
-//    }
-//
-//    //  the update employee REST API
-//    @PutMapping("{id}")
-//    public ResponseEntity<User> updateEmployee(@PathVariable long id, @RequestBody User users) {
-//        User updateEmployee = userRepository.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException(" Employee not exist with id: " + id));
-//
-//
-//
-//        updateUser.setUsername( users.getUsername());
-//        updateUser.setName( users.getName());
-//        updateUser.setEmailId( users.getEmailId());
-//
-//        updateUser.setPhone( users.getPhone());
-//        updateUser.setWebsite( users.getWebsite());
-//        updateUser.setAddress( users.getAddress());
-//        updateUser.setCompany( users.getCompany());
-//
-//        userRepository.save(updateUser);
-//        return ResponseEntity.ok(updateUser);
-//    }
-//
-//    // delete employee REST API
-//    @DeleteMapping("{id}")
-//    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id) {
-//
-//        User user = userRepository.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException(" the Employe being serached does not exist with id: " + id));
-//
-//        userRepository.delete(user);
-//
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//
-//    }
 
